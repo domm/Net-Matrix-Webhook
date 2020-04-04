1cacc05c6d8b83078b9594bf8438d838f97ecd9f
Thomas Klausner <domm@plix.at>
2020-04-05 00:12:05 +0200

    add commandline opts

--- bin/http2matrix.pl
+++ bin/http2matrix.pl
@@ -2,24 +2,18 @@
 use strict;
 use warnings;
 
+use Getopt::Long;
 use Log::Any::Adapter ( $ENV{LOGADAPTER} || 'Stdout', log_level => $ENV{LOGLEVEL} || 'info' );
 use Net::Matrix::Webhook;
 
-#use Getopt::Long;
-#my $data   = "file.dat";
-#my $length = 24;
-#my $verbose;
-#GetOptions ("length=i" => \$length,    # numeric
-#            "file=s"   => \$data,      # string
-#            "verbose"  => \$verbose)   # flag
-#or die("Error in command line arguments\n");
-#
-Net::Matrix::Webhook->new({
-    matrix_home_server => $ENV{MATRIX_HOME_SERVER},
-    matrix_user => $ENV{MATRIX_USER},
-    matrix_password => $ENV{MATRIX_PASSWORD},
-    http_port => $ENV{HTTP_PORT} || 8765,
-    secret=>$ENV{SECRET},
-})->run;
+my %opts = (
+    'matrix_home_server' => $ENV{MATRIX_HOME_SERVER},
+    'matrix_user'        => $ENV{MATRIX_USER},
+    'matrix_password'    => $ENV{MATRIX_PASSWORD},
+    'http_port'          => $ENV{HTTP_PORT} || 8765,
+    'secret'             => $ENV{SECRET},
+);
+GetOptions( \%opts, qw(matrix_home_server=s matrix_user=s matrix_password=s http_port:i secret:s) );
 
+Net::Matrix::Webhook->new( \%opts )->run;
 
