f7f3cc024f70917d9ace54c210ced158db8a7a17
Thomas Klausner <domm@plix.at>
2020-04-09 23:28:05 +0200

    autocreate cpanfile, --matrix_room

--- bin/http2matrix.pl
+++ bin/http2matrix.pl
@@ -17,7 +17,7 @@ my %opts = (
     'http_port'          => $ENV{HTTP_PORT} || 8765,
     'secret'             => $ENV{SECRET},
 );
-GetOptions( \%opts, qw(matrix_home_server=s matrix_user=s matrix_password=s http_port:i secret:s) );
+GetOptions( \%opts, qw(matrix_home_server=s matrix_room=s matrix_user=s matrix_password=s http_port:i secret:s) );
 
 Net::Matrix::Webhook->new( \%opts )->run;
 
--- cpanfile
+++
@@ -1,5 +0,0 @@
-requires 'Net::Async::Matrix';
-requires 'Net::Async::HTTP::Server';
-requires 'IO::Async::SSL';
-requires 'Digest::SHA1';
-requires 'Plack';
